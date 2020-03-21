7acd3ed20fce0b8d479f470e84818b3be7991505
Jackson-Kim <43841846+Jackson-Kim@users.noreply.github.com>
2020-03-21 17:34:58 +0900

    python-venv install django
    python 가상환경 구축 및 django 설치

---
+++ virtualSpace-1/myvenv-1/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\pino\desktop\mydjango\virtualspace-1\myvenv-1\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
